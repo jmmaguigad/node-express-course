16a5906a88a01e6c66bc590840e1ef4bf76f4adb
JM Maguigad <jmmaguigad@users.noreply.github.com>
2020-04-12 00:34:26 +0800

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const mockUserData=[
