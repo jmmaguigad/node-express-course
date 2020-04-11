cab12c8f5aeafc70d091e1ac124979495f548166
JM Maguigad <jmmaguigad@users.noreply.github.com>
2020-04-12 00:36:15 +0800

    add login POST route

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/users/:id',function(req,res){
 
 app.post('/login',function(req,res){
 	const username = req.body.username;
-	const passwrod = req.body.password;
+	const password = req.body.password;
 
 	const mockUsername = "billyTheKid";
 	const mockPassword = "superSecret";
